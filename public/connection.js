7b9e8da5da7552eb8d3db55be39b8698dbef0ea6
grptrsmng <128395606+grptrsmng@users.noreply.github.com>
2023-04-13 05:09:31 +0700

    Update connection.js

--- public/connection.js
+++ public/connection.js
@@ -10,7 +10,7 @@ class TikTokIOConnection {
 
         this.socket.on('connect', () => {
             console.info("Socket connected!");
-            alert("connected");
+
             // Reconnect to streamer if uniqueId already set
             if (this.uniqueId) {
                 this.setUniqueId();
